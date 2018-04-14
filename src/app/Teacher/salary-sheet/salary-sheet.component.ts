e16ea3c877f820b000e3b16629d1126b0c16a179
Oshara <oshara.16@cse.mrt.ac.lk>
2018-04-15 00:24:52 +0530

    git push

---
+++ src/app/Teacher/salary-sheet/salary-sheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-sheet',
+  templateUrl: './salary-sheet.component.html',
+  styleUrls: ['./salary-sheet.component.css']
+})
+export class SalarySheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
