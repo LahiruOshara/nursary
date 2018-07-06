9eaaf9ad89be08c0590db709dc20a8fd54ee75ae
Oshara <oshara.16@cse.mrt.ac.lk>
2018-07-06 23:01:25 +0530

    git push

--- src/app/Admin/declareholiday/declareholiday.component.ts
+++ src/app/Admin/declareholiday/declareholiday.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-declareholiday',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeclareholidayComponent implements OnInit {
 
-  constructor() { }
+  date: String;
+  reason: String;
+
+  constructor(private validateService: ValidateService,
+    private messages: FlashMessagesService ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    const msg = {
+      date: this.date,
+      reason: this.reason
+    };
+
+    // validate date
+    if (!this.validateService.validateDate(msg.date)) {
+      this.messages.show( 'Date is not valid', {
+        cssClass: 'alert-danger',
+        timeOut: 5000 });
+        return false;
+    }
   }
 
 }
